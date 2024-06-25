d60ac8c1893ddef14782dae62d56ad5cf587b6cf
Samuel Huf <samhuf@seznam.cz>
2024-06-25 11:51:37 +0200

    Finalni verze, jeste bude dodelany proklik na portfolio

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
+let museumsGroup = createClusterGroup();
+let shopsGroup = createClusterGroup();
+
+function createTileLayer(url, maxZoom, attribution) {
+    return L.tileLayer(url, {
+        maxZoom: maxZoom,
+        attribution: attribution,
+    });
+}
+
+let osm = createTileLayer(
+    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    18,
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+);
+
+let ortofoto = createTileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    18,
+    "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+);
+
 let map = L.map("map", {
     center: [49.8037633, 15.4749126],
     zoom: 7,
+    layers: [osm, museumsGroup, shopsGroup],
 });
 
-let osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-});
+let baseMaps = {
+    OSM: osm,
+    Ortofoto: ortofoto,
+};
 
-let ortofoto = L.tileLayer(
-    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-    {
-        maxZoom: 19,
-        attribution:
-            "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+let layerControl = L.control.layers(baseMaps, null).addTo(map);
+
+let IconClass = L.Icon.extend({
+    options: {
+        iconSize: [30, 30],
+        iconAnchor: [15, 15],
+        popupAnchor: [0, -5],
     },
-);
-
-osm.addTo(map);
-
-let museumIcon = L.icon({
-    iconUrl: "./img/museum.png",
-
-    iconSize: [30, 30],
-    iconAnchor: [25, 25],
 });
 
-let shopIcon = L.icon({
-    iconUrl: "./img/shop.png",
-    iconSize: [30, 30],
-    iconAnchor: [25, 25],
-});
+let museumIcon = new IconClass({ iconUrl: "./img/museum.png" });
+
+let shopIcon = new IconClass({ iconUrl: "./img/shop.png" });
 
 async function getData() {
     let museums = await fetch("./data/museums.json").then((response) =>
@@ -42,11 +54,33 @@ async function getData() {
         response.json(),
     );
 
-    showData(museums, shops);
+    showData(museums, museumsGroup, museumIcon);
+    showData(shops, shopsGroup, shopIcon);
+
+    layerControl.addOverlay(museumsGroup, "Muzea");
+    layerControl.addOverlay(shopsGroup, "Prodejci");
 }
 
-function showData(museums, shops) {
-    let dataGroup = L.markerClusterGroup({
+function showData(items, group, icon) {
+    items.forEach((item) => {
+        group.addLayer(
+            L.marker([item.lat, item.lng], {
+                icon: icon,
+                title: item.title,
+            }).bindPopup(
+                "<b>Název:</b> " +
+                    item.title +
+                    "<br><b>Adresa:</b> " +
+                    item.address +
+                    "<br><b>Město:</b> " +
+                    item.city,
+            ),
+        );
+    });
+}
+
+function createClusterGroup() {
+    return L.markerClusterGroup({
         maxClusterRadius: 30,
         iconCreateFunction: function (cluster) {
             return L.divIcon({
@@ -57,17 +91,6 @@ function showData(museums, shops) {
         },
         showCoverageOnHover: false,
     });
-    museums.forEach((museum) => {
-        dataGroup.addLayer(
-            L.marker([museum.lat, museum.lng], { icon: museumIcon }),
-        );
-    });
-
-    shops.forEach((shop) => {
-        dataGroup.addLayer(L.marker([shop.lat, shop.lng], { icon: shopIcon }));
-    });
-
-    map.addLayer(dataGroup);
 }
 
 getData();
